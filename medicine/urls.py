a40daf0dcb3b620e0328db8eeae1079d3eb78a0d
chiseledeagle <79354943+chiseledeagle@users.noreply.github.com>
2022-01-29 01:12:16 +0530

    Update urls.py

--- medicine/urls.py
+++ medicine/urls.py
@@ -1,13 +1,24 @@
-from django.urls import path
-
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.medicinesfind, name='medicinesfind'),
+    path('',views.heading, name='home'),
+    path('patient', views.patient, name="patient"),
+    path('home.html',views.home),
     path('search',views.search, name='search'),
-    path('pharmacy.html',views.pharmacy),
-    path('medicinesfind.html',views.medicinesfind),
-    path('medsfind.php',views.medsfind),
-
-
+    path('searchhelp',views.searchhelp, name='searchhelp'),
+    path('searchco',views.searchco, name='searchco'),
+    path('searchs',views.searchs, name='searchs'),
+    path('Find_Healthcare.html',views.pharmacy),
+    path('Find_Medicine.html',views.medicinesfind),
+    path('Customer_care.html',views.custom),
+    path('result3.html',views.patient),
+    path('reminder.html',views.reminder),
+    path('home2.html',views.home2),
+    path('Shopping.html',views.shopping),
+    path('Search_Result.html',views.searchco),
+    path('signup.html',views.signup),
+    path('login.html',views.login),
+    path('signup',views.signup),
+    path('login',views.login)
 ]
